cf224833a461a265450377d7b72755077a35b9fa
Akshita <mathur_akshita@yahoo.ca>
2025-04-16 14:47:18 -0400

    Initial setup
    - colours decided
    - hero image created
    - basic data json file added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import {Container, Row, Col} from 'react-bootstrap';
+import data from './json/data.json';
+
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Row>
+            <Col className="App-introtile tile-dark" md={{ span: 8, offset: 4 }}>
+              <center><h1>{data.basics.name}</h1>
+              <h3>Web Developer, Toronto, Ontario</h3></center>
+              <p>{data.basics.summary}</p>
+            </Col>
+          </Row>
+        </Container>
+       
+        
+       <p>This is an example of what the text will look like.</p>
+       <h2>This is an example of the title.</h2>
+       {2+2}
+        
       </header>
     </div>
   );
